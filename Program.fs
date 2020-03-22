75e964243736a5b3d6dd5d345420619ec74983c5
Alican Demirtas <alican@compositional-it.com>
2020-03-22 12:48:46 +0000

    Basic template created with the files in https://github.com/CompositionalIT/basic-saturn

---
+++ Program.fs
@@ -0,0 +1,13 @@
+﻿open Saturn
+open Giraffe
+
+let myRouter = router {
+    get "/api/foo" (text "Hello")
+    get "/api/bar" (json {| Name = "Frodo"; Age = 60 |})
+}
+
+let app = application {
+    use_router myRouter
+}
+
+run app
